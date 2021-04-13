f20a9a7565f7b1c889cff9e019e2fc6c975aac75
KishoreMayank <mayankkishore.in@gmail.com>
2021-04-13 12:59:33 -0400

    working on video processor

--- test_simple.py
+++ test_simple.py
@@ -14,6 +14,7 @@ import numpy as np
 import PIL.Image as pil
 import matplotlib as mpl
 import matplotlib.cm as cm
+import time
 
 import torch
 from torchvision import transforms, datasets
@@ -151,5 +152,7 @@ def test_simple(args):
 
 
 if __name__ == '__main__':
+    start = time.time()
     args = parse_args()
     test_simple(args)
+    print("time taken: ", time.time() - start)
